56f8fffb2812691782bcc990cbbea4dc03e6ce6e
Alair Fonseca <alairfonseca13@gmail.com>
2017-05-07 14:00:25 -0300

    finalizando o curso

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -13,13 +13,37 @@ int main() {
   int ganhou = 0;
   int chute;
 
+  int acertou = 0;
+
   int segundos = time(0);
   srand(segundos);
 
   int random = rand();
   int numerosecreto = random % 100;
 
-  while (!ganhou) {
+  int numerodetentativas;
+  int nivel;
+
+  printf("Qual o nível de dificuldade?\n");
+  printf("(1) fácil, (2) médio, (3) difícil)\n");
+  printf("Escolha: ");
+  scanf("%d\n", &nivel);
+
+  switch (nivel) {
+    case 1:
+      numerodetentativas = 20;
+    break;
+
+    case 2:
+      numerodetentativas = 15;
+    break;
+
+    default:
+      numerodetentativas = 6;
+    break;
+  }
+
+  for (int i = 1; i <= numerodetentativas; i ++) {
     printf("Tentativa : %d\n", contador + 1);
 
     printf("Qual e o seu chute ?\n");
@@ -31,20 +55,19 @@ int main() {
       continue;
     }
 
-    int acertou = (chute == numerosecreto);
+    acertou = (chute == numerosecreto);
     int maior = (chute > numerosecreto);
 
     if (acertou) {
-        printf("Parabens! Você acertou!\n");
-        ganhou = 1;
+        break;
     } else if (maior) {
         printf("Você errou!\n");
         printf("O número secreto é menor que o número informado.\n");
-        printf("Tente novamente.\n");
+        printf("Tente novamente.\n\n\n");
     } else {
         printf("Você errou!\n");
         printf("O número secreto é maior que o número informado.\n");
-        printf("Tente novamente.\n");
+        printf("Tente novamente.\n\n\n");
     }
 
     contador++;
@@ -53,8 +76,14 @@ int main() {
     pontos -= pontosperdidos;
   }
 
+
+  if (acertou) {
+    printf("Parabens! Você acertou!\n");
+    printf("Você acertou em %d tentativas\n", contador);
+    printf("Total de pontos %.2f\n", pontos);
+  } else {
+    printf("Você perdeu! Tente novamente.\n");
+  }
   printf("\n    **Fim de jogo**");
-  printf("Você acertou em %d tentativas\n", contador);
-  printf("Total de pontos %.2f\n", pontos);
 
 }
